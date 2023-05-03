92f42d38a5cfecd89163749fd46fbeeafcaee7d8
Maksym-Feshchenko <maksym.fd@gmai.com>
2023-05-03 21:49:50 +0300

    delete import Component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
 import Statistics from './Statistics/Statistics';
 
